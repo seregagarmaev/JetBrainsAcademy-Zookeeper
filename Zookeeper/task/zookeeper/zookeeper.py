e58f9293b03f0fdb95f9871be4d7392017c592c4
seregagarmaev <garmaevs@gmail.com>
2020-09-10 17:41:47 +0700

    Stage 2/4 completed

--- Zookeeper/task/zookeeper/zookeeper.py
+++ Zookeeper/task/zookeeper/zookeeper.py
@@ -1,5 +1,20 @@
-print('I love animals!')
-print("Let's check on the animals...")
-print('The deer looks fine.')
-print('The bat looks happy.')
-print('The lion looks healthy.')
+print(r"""
+Switching on the camera in the camel habitat...
+ ___.-''''-.
+/___  @    |
+',,,,.     |         _.'''''''._
+     '     |        /           \
+     |     \    _.-'             \
+     |      '.-'                  '-.
+     |                               ',
+     |                                '',
+      ',,-,                           ':;
+           ',,| ;,,                 ,' ;;
+              ! ; !'',,,',',,,,'!  ;   ;:
+             : ;  ! !       ! ! ;  ;   :;
+             ; ;   ! !      ! !  ; ;   ;,
+            ; ;    ! !     ! !   ; ;
+            ; ;    ! !    ! !     ; ;
+           ;,,      !,!   !,!     ;,;
+           /_I      L_I   L_I     /_I
+Look at that! Our little camel is sunbathing!""")
